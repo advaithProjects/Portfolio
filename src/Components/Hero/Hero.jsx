d7e4c4977ef988b72c563f0619094a4b42a1090b
advaithProjects <advaithvijjagiri@gmail.com>
2026-03-06 21:59:34 +0530

    06/03/2026 10:oopm commit

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import profile_img from "../../assets/profile_img.svg";
+import "./Hero.css";
+const Hero = () => {
+  return (
+    <div className="hero">
+      <img src={profile_img} alt="" />
+      <h1>
+        <span>I'm Vijjagiri Sai Advaithananda,</span>frontend developer
+      </h1>
+      <p>I am a Fresher</p>
+      <div className="hero-action">
+        <div className="hero-connect">Connect with me</div>
+        <div className="hero-resume">My resume</div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
